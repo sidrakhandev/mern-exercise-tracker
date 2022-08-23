7d1fc285cbeefc66c2d65fb0512507af37726a62
sidrakhandev <sidrakhan.me.100@gmail.com>
2022-08-23 12:24:39 +0500

    connecting frontend with backend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercises.component";
@@ -15,15 +11,15 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <BrowserRouter>
-    <div className="container">
-      <Navbar/>
-      <br/>
-      <Routes>
-      <Route path="/" element={<ExercisesList/>} exact />
-      <Route path="/edit/:id" element={<EditExercise/>}/>
-      <Route  path="/create" element={<CreateExercise/>}/>
-      <Route path="/user" element={<CreateUser/>}/>
-      </Routes>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Routes>
+          <Route path="/" element={<ExercisesList />} exact />
+          <Route path="/edit/:id" element={<EditExercise />} />
+          <Route path="/create" element={<CreateExercise />} />
+          <Route path="/user" element={<CreateUser />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DatePicker from "react-datepicker";
+import axios from "axios";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercises extends Component {
@@ -20,10 +21,16 @@ export default class CreateExercises extends Component {
     };
   }
   componentDidMount() {
-    this.setState({
-      users: ["test user"],
-      username: "test user",
-    });
+   axios.get("http://localhost:5000/users/")
+        .then((res) => {
+            if (res.data.length > 0) {
+                this.setState({
+                users: res.data.map((user) => user.username),
+                username: res.data[0].username
+                });
+            }
+            }
+        )
   }
   onChangeUsername = (e) => {
     this.setState({
@@ -54,6 +61,9 @@ export default class CreateExercises extends Component {
       date: this.state.date,
     };
     console.log(exercise);
+    axios
+      .post("http://localhost:5000/exercises/add", exercise)
+      .then((res) => console.log(res.data));
     window.location = "/";
   };
   render() {
